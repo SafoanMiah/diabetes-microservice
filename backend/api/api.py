5620add10c626daefe0f0eee37597412a8a5e52b
DrDaddySaf <saf.home.2021@gmail.com>
2025-03-19 14:14:20 +0000

    initial setup

---
+++ backend/api/api.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+class DataAPI:
+    """Class for the Flask API"""
+    pass
---
+++ backend/data/loader.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+class DataLoader:
+    """Class to load data with"""
+    pass
