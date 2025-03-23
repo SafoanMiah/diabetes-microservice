06615fea10cafe404778dc1b7884e1ec93d11076
Matilda Bentes Edwards <mb31102@nulondon.ac.uk>
2025-03-23 17:28:22 +0000

    Loaded Dara into pandas dataframe

--- backend/data/loader.py
+++ backend/data/loader.py
@@ -4,6 +4,12 @@ from sklearn.datasets import load_diabetes
 
 diabetes = load_diabetes() #this is a json file so we can't load it directly as is
 
+x = diabetes.data
+y = diabetes.target #literally refers to target which is the most important column
+                    #in terms of model training
+df = pd.DataFrame(x, columns = diabetes.feature_names)
+
+df["target"] = y
 
 
 
