81a5ad85c25ddcd5a4ff6a7aea4895619c64add4
Matilda Bentes Edwards <mb31102@nulondon.ac.uk>
2025-03-23 17:37:56 +0000

    Load data into pandas dataframe (with comments)

--- backend/data/loader.py
+++ backend/data/loader.py
@@ -4,12 +4,12 @@ from sklearn.datasets import load_diabetes
 
 diabetes = load_diabetes() #this is a json file so we can't load it directly as is
 
-x = diabetes.data
+x = diabetes.data #series of arrays
 y = diabetes.target #literally refers to target which is the most important column
                     #in terms of model training
-df = pd.DataFrame(x, columns = diabetes.feature_names)
+df = pd.DataFrame(x, columns = diabetes.feature_names) #creates pandas dataframe out of the arrays in x
 
-df["target"] = y
+df["target"] = y #creates target column with target data in it which was not in x
 
 
 
