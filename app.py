d1af064ce33106e6927155902e9b868887925df9
Saf <71877181+SafoanMiah@users.noreply.github.com>
2025-03-24 19:20:16 +0000

    altair start

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/altair")
+def altair():
+    return render_template("altair.html")
+
+
 if __name__ == '__main__':
     app.run()
