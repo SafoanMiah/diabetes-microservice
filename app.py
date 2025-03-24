2bb4c1c69492dcd05d522c001b247668f99505f0
Saf <71877181+SafoanMiah@users.noreply.github.com>
2025-03-24 20:22:45 +0000

    basic front end completion

--- app.py
+++ app.py
@@ -9,10 +9,5 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/altair")
-def altair():
-    return render_template("altair.html")
-
-
 if __name__ == '__main__':
     app.run()
