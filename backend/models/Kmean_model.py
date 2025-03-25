4ee578093ef74a8f0cd76d4fa03a70533e2fb46a
Matilda Bentes Edwards <mb31102@nulondon.ac.uk>
2025-03-25 11:22:28 +0000

    Changed file name to regression

--- backend/models/Kmean_model.py
+++
@@ -1,4 +0,0 @@
-#split the dataset into test and training. Saf wants us to split test into validation and test so that we can do our testing in a more refined manner.
-#we're using a kmean clustering model due to the assignment's visualisation requirements want a visualisation with clusters and their centres.
-#we did debate using a knn model (Tili) but saf suggested kmean clustering as this could give us a centre.
-
