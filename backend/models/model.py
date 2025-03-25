c98739ff177306595cb946c28ac4e097af0f6129
Matilda Bentes Edwards <mb31102@nulondon.ac.uk>
2025-03-25 19:56:26 +0000

    split model into training and testing data

--- backend/models/model.py
+++ backend/models/model.py
@@ -1,1 +1,13 @@
-from backend.data import preprocessing
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+diabetes_df =  pd.read_csv("backend/data/datasets/diabetes.csv")
+
+X = diabetes_df.drop("target", axis = 1)
+y = diabetes_df["target"]
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.25, random_state = 354)
+
+
+
+
