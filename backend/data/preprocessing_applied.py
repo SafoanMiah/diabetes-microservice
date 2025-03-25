9b91e68520ac64de9157f90059eb3a70ddad4c05
SafoanMiah <saf.home.2017@gmail.com>
2025-03-25 12:04:45 +0000

    updates

--- backend/data/preprocessing_applied.py
+++ backend/data/preprocessing_applied.py
@@ -1,10 +1,25 @@
 #this is the implementation of the cleaning. It allows us to fiddle with how we want to use the builder
 #to cut up the code. I haven't used drop_column() but it would look like this: .drop_column("column name")
 
-import preprocessing as pp
+from preprocessing import DataPreProcessingBuilder
 import loader
+import pandas as pd
 
+# load the dataset
 df = loader.df
-builder = pp.DataPreProcessingBuilder()
 
-cleaned_df = builder.load_df(df).null_data().remove_duplicates().to_float().white_spaces().build()
+# init builder
+builder = DataPreProcessingBuilder()
+
+# Preprocessing steps: handle null values, remove duplicates, convert to float, trim whitespaces
+cleaned_df = (
+    builder
+    .load_df(df)
+    .null_data()
+    .remove_duplicates()
+    .to_float()
+    .white_spaces()
+    .build()
+)
+
+print(cleaned_df.co)
