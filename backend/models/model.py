d6fb7a3c186859e702cffae078f904e3ecd47926
Matilda Bentes Edwards <mb31102@nulondon.ac.uk>
2025-03-25 20:01:41 +0000

    save model to pickle

--- backend/models/model.py
+++ backend/models/model.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
@@ -11,5 +12,9 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.25, rando
 
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+with open("linear_regression_model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
 
 
