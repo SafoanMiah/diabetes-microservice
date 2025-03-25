65949dc1baadd7a2c205d3ea267988204122d072
Matilda Bentes Edwards <mb31102@nulondon.ac.uk>
2025-03-25 19:58:12 +0000

    train models

--- backend/models/model.py
+++ backend/models/model.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 diabetes_df =  pd.read_csv("backend/data/datasets/diabetes.csv")
@@ -8,6 +9,7 @@ X = diabetes_df.drop("target", axis = 1)
 y = diabetes_df["target"]
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.25, random_state = 354)
 
-
+model = LinearRegression()
+model.fit(X_train, y_train)
 
 
