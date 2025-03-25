2751d7f31c23311bee7fd110ea4c2c9cb5260159
Saf <71877181+SafoanMiah@users.noreply.github.com>
2025-03-25 19:38:36 +0000

    application of rename + bugfixes, removed inplace = True

--- backend/data/preprocessing_applied.py
+++ backend/data/preprocessing_applied.py
@@ -8,11 +8,27 @@ import pandas as pd
 # load the dataset
 df = loader.df
 
+# init builder
 builder = DataPreProcessingBuilder()
 
-cleaning = builder.load_df(df).null_data().remove_duplicates().rename_column("s1", "tc").to_float().build()
-cleaned_df = cleaning.clean()
+# Preprocessing steps: handle null values, remove duplicates, convert to float
+cleaned_df = (
+    builder
+    .load_df(df)
+    .null_data()
+    .remove_duplicates()
+    .to_float()
+
+    # Rename columns, to: 'age', 'sex', 'bmi', 'bp', 'tc', 'ldl', 'hdl', 'tch', 'ltg', 'glu'
+    # https://scikit-learn.org/stable/datasets/toy_dataset.html
+    .rename_column('s1', 'tc')
+    .rename_column('s2', 'ldl')
+    .rename_column('s3', 'hdl')
+    .rename_column('s4', 'tch')
+    .rename_column('s5', 'ltg')
+    .rename_column('s6', 'glu')
+    .build()
+)
 
 # Saving the cleaned datafram to a CSV
-cleaned_df.to_csv("datasets/diabetes.csv", index=False)
-
+cleaned_df.to_csv("backend/data/datasets/diabetes.csv", index=False)
